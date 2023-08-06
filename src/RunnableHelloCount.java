3a082c67cad35c9556ed11655f2ff5e49946846c
nyasimi23 <the nextrenaissance892@gmail.com>
2023-08-06 21:33:39 +0300

    v1

---
+++ src/RunnableHelloCount.java
@@ -0,0 +1,40 @@
+public class RunnableHelloCount implements Runnable {
+
+    // Properties
+    private Thread thread1;
+    private Thread thread2;
+
+    public static void main(String[] args) {
+        RunnableHelloCount threadDemo = new RunnableHelloCount();
+    }
+
+    public RunnableHelloCount() {
+        // Pass the RunnableHelloCount instance to the Thread constructor
+        thread1 = new Thread(this);
+        thread2 = new Thread(this);
+
+        thread1.start();
+        thread2.start();
+    }
+
+    @Override
+    public void run() {
+        int pause;
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                // use static method currentThread to get reference
+                // to current thread and then call method getName
+                // on that reference
+                System.out.println(Thread.currentThread().getName() + " being executed!");
+
+                pause = (int) (Math.random() * 3000);
+
+                // call the static method sleep
+                Thread.sleep(pause);
+            } catch (InterruptedException interExce) {
+                System.out.println(interExce);
+            }
+        }
+    }
+}
---
+++ src/RunnableShowName.java
@@ -0,0 +1,34 @@
+// Listing 3: Using the runnable Interface
+
+public class RunnableShowName implements Runnable{
+
+    public static void main(String[] args) {
+        
+        Thread thread1 = new Thread(new RunnableShowName());
+        Thread thread2 = new Thread(new RunnableShowName());
+        
+        thread1.start();
+        thread2.start();
+    }
+
+    public void run(){
+        int pause;
+         
+        for(int i = 0; i < 10; i++){
+            try {
+                // use static method currentThread to get reference
+                // to current thread and then call method getName
+                // on that reference
+                System.out.println(Thread.currentThread().getName() + " being executed!");
+
+                pause = (int)(Math.random() * 3000);
+
+                // call the static method sleep
+                Thread.sleep(pause);
+            } catch (InterruptedException interExce) {
+                System.out.println(interExce);
+            }
+        }
+    }
+    
+}
